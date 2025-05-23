7c163c4f0606733910277144d3bdcb620fb3e070
MatthewKK01 <mateyavelashvili@gmail.com>
2025-05-23 08:34:09 +0400

    i18n-core features

--- src/components/sections/Features.jsx
+++ src/components/sections/Features.jsx
@@ -3,9 +3,14 @@ import Data from "@data/sections/features.json";
 import Accordion from 'react-bootstrap/Accordion';
 import ModalVideo from 'react-modal-video';
 import 'react-modal-video/css/modal-video.css';
+import { useTranslation } from "next-i18next";
 
 const FeaturesSection = () => {
   const [isOpen, setOpen] = useState(false);
+
+  const { t } = useTranslation('features');
+  const Data = t('items', { returnObjects: true });
+  console.log(Data)
 
   return (
     <section className="core-features">
@@ -18,8 +23,8 @@ const FeaturesSection = () => {
                     <div className="row">
                     <div className="col-lg-12">
                         <div className="data">
-                            <span>{Data.subtitle}</span>
-                            <h2>{Data.title}</h2>
+                            <span>{t("subtitle")}</span>
+                            <h2>{t("title")}</h2>
                         </div>
                     </div>         
                     </div>
@@ -27,7 +32,7 @@ const FeaturesSection = () => {
                 </div>
 
                 <Accordion defaultActiveKey="features-acc-0">
-                    {Data.items.map((item, key) => (
+                    {Data.map((item, key) => (
                     <Accordion.Item key={`features-item-${key}`} eventKey={`features-acc-${key}`}>
                         <Accordion.Header>
                             <span className="num">{item.num}</span> {item.title}
@@ -45,7 +50,7 @@ const FeaturesSection = () => {
                 <div className="shape">
                     <div className="video">
                         <figure>
-                            <img src={Data.image.url} alt={Data.image.alt} />
+                            <img src={'/img/core-feature.jpeg'} alt={'image'} />
                         </figure>
                         <button className="video-play-btn" onClick={() => setOpen(true)} style={{ "cursor" : "pointer" }}>
                             <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="35" height="56" viewBox="0 0 35 56"> <defs> <clipPath id="clip-video_arrow"> <rect width="35" height="56"/> </clipPath> </defs> <g id="video_arrow" data-name="video arrow" clipPath="url(#clip-video_arrow)"> <path id="Shape_1" data-name="Shape 1" d="M1362,5000.8,1327,4972V5027Z" transform="translate(-1326.998 -4971.996)" fill="rgba(0,0,0,0)"/> <path id="Shape_1_-_Outline" data-name="Shape 1 - Outline" d="M1333,5015.017l19.29-14.437L1333,4984.7v30.313M1327,5027V4972l35,28.807Z" transform="translate(-1326.998 -4971.996)"/> </g> </svg>
@@ -56,7 +61,7 @@ const FeaturesSection = () => {
         </div>
         </div>
 
-        <ModalVideo channel='youtube' isOpen={isOpen} videoId={Data.yt_video_id} onClose={() => setOpen(false)} />
+        <ModalVideo channel='youtube' isOpen={isOpen} videoId={"uemObN8_dcw"} onClose={() => setOpen(false)} />
     </section>
   );
 };
